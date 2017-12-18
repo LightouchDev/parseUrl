802dd24764eb59739129061b486652a028b8b5f1
Miau Lightouch <miaulightouch@gmail.com>
2017-12-18 23:09:49 +0800

    Add URL class example

--- example.js
+++ example.js
@@ -1,9 +1,14 @@
 'use strict'
 
 const parseUrl = require('.')
+const { URL } = parseUrl
 
 const testUrl = 'http://jblas:password@mycompany.com:8080/mail/inbox?msg=1234&type=unread#msg-content'
 
 console.log(parseUrl(testUrl))
 console.log(parseUrl('http://example.com/foo'))
-console.log(parseUrl('//jblas:password@mycompany.com:8080/mail/inbox?msg=1234&type=unread#msg-content')) // Throw error
+
+console.log(new URL(testUrl))
+console.log(new URL('http://example.com/foo'))
+
+// console.log(parseUrl('//jblas:password@mycompany.com:8080/mail/inbox?msg=1234&type=unread#msg-content')) // Throw error
